915a7d99abc0b4580182606a9193a49486bc2bb5
arulgupta <arul18.gupta@gmail.com>
2018-03-22 16:20:01 -0400

    Initial check-in of module apptesting

---
+++ apptesting/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace apptesting
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
